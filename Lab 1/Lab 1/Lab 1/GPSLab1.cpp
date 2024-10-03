59f17451b62d4e385ed4bf68dbe78c99800903e0
Simina Dan-Marius <dansimina10@gmail.com>
2024-10-03 21:51:14 +0300

    lab1

--- Lab 1/Lab 1/Lab 1/GPSLab1.cpp
+++ Lab 1/Lab 1/Lab 1/GPSLab1.cpp
@@ -27,14 +27,47 @@ namespace gps {
     
     bool IsConvex(const std::vector<glm::vec2> &vertices)
     {
-        /*for (int i = 0; i < vertices.size() - 2; i++) {
-            
-        }*/
+        for (int i = 0; i < vertices.size() - 1; i++) 
+        {
+            if (i < vertices.size() - 2) {
+                glm::vec3 e1 = glm::vec3(vertices[i + 1].x - vertices[i].x, vertices[i + 1].y - vertices[i].y, 0);
+                glm::vec3 e2 = glm::vec3(vertices[i + 2].x - vertices[i + 1].x, vertices[i + 2].y - vertices[i + 1].y, 0);
+
+                if (glm::cross(e1, e2).z < 0) {
+                    return false;
+                }
+            }
+            else {
+                glm::vec3 e1 = glm::vec3(vertices[i + 1].x - vertices[i].x, vertices[i + 1].y - vertices[i].y, 0);
+                glm::vec3 e2 = glm::vec3(vertices[0].x - vertices[i + 1].x, vertices[0].y - vertices[i + 1].y, 0);
+
+                if (glm::cross(e1, e2).z < 0) {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
     }
     
     std::vector<glm::vec2> ComputeNormals(const std::vector<glm::vec2> &vertices)
     {
         std::vector<glm::vec2> normalsList;
+
+        for (int i = 0; i < vertices.size(); i++) {
+            if (i < vertices.size() - 1) {
+                glm::vec3 edge = glm::vec3(vertices[i + 1].x - vertices[i].x, vertices[i + 1].y - vertices[i].y, 0);
+                glm::vec3 crossProduct = glm::cross(edge, glm::vec3(0, 0, 1));
+                normalsList.push_back(glm::normalize(glm::vec2(crossProduct.x, crossProduct.y)));
+            }
+            else {
+                glm::vec3 edge = glm::vec3(vertices[0].x - vertices[i].x, vertices[0].y - vertices[i].y, 0);
+                glm::vec3 crossProduct = glm::cross(edge, glm::vec3(0, 0, 1));
+                normalsList.push_back(glm::normalize(glm::vec2(crossProduct.x, crossProduct.y)));
+            }
+        }
+
         return normalsList;
     }
 }
